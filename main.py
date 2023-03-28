8385ccf14918ea7989db2036159e803b3f78c67d
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-28 17:00:47 +0000

    done

---
+++ main.py
@@ -0,0 +1,8 @@
+L = int(input("The Length of Right Rectangular Prism:  "))
+W = int(input("The Width of Right Rectangular Prism:  "))
+H = int(input("The Height of Right Rectangular Prism:  "))
+
+volume = int(L) * int(W) * int(H)
+
+print("The volume of Right Rectangular Prism is  ")
+print(volume)
